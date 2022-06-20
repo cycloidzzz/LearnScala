f32b79fba2b840a0e3ccb874ff4d54ef886b377b
cycloidzzz <cycloidz@126.com>
2022-06-21 01:03:12 +0800

    feat(SimplePractice): add a more functional method with flatMap and map for CartesianProduct Generator

--- SimplePractice/src/main/scala/CartesianProduct.scala
+++ SimplePractice/src/main/scala/CartesianProduct.scala
@@ -6,16 +6,26 @@ object CartesianProduct {
   }
 
   def enumerate[T](sequences: Seq[Seq[T]]): Seq[Seq[T]] = {
+
     def productInner(streams: Seq[Seq[T]]): Seq[Seq[T]] = streams match {
       case Seq(first) => {
         for (a <- first)
-          yield Seq(a)
+         yield Seq(a)
       }
       case Seq(first, rest @_*) => {
         for (a <- first; b <- productInner(rest))
-          yield a +: b
+         yield a +: b
+     }
+    }
+
+    def productInnerWithFlatMap(streams: Seq[Seq[T]]): Seq[Seq[T]] = streams match {
+      case Seq() => Seq(Seq())
+      case Seq(first, rest @_*) => {
+        val rest_product: Seq[Seq[T]] = productInnerWithFlatMap(rest);
+        first.flatMap((a: T) => rest_product.map(a +: _))
       }
     }
-    productInner(sequences)
+    
+    productInnerWithFlatMap(sequences)
   }
 }
